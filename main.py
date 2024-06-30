5e448d098685e54d028456efc1a76bc2fb533af6
TriaX <bagamag12@gmail.com>
2024-07-01 02:54:54 +0300

    pep 8

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 Base.metadata.create_all(bind=engine)
 
+
 def get_db():
     db = SessionLocal()
     try:
@@ -20,14 +21,17 @@ def get_db():
     finally:
         db.close()
 
+
 @app.get("/", response_class=HTMLResponse)
 async def main():
     return templates.TemplateResponse("main_page.html", {"request": {}})
+
 
 @app.get("/get_users", response_class=HTMLResponse)
 async def get_users(request: Request, db: Session = Depends(get_db)):
     users = db.query(User).all()
     return templates.TemplateResponse("get_users.html", {"request": request, "users": users})
+
 
 @app.get("/user/{user_id}", response_class=HTMLResponse)
 async def view_profile(request: Request, user_id: int, db: Session = Depends(get_db)):
@@ -36,6 +40,7 @@ async def view_profile(request: Request, user_id: int, db: Session = Depends(get
         raise HTTPException(status_code=404, detail="User not found")
     return templates.TemplateResponse("profile.html", {"request": request, "user": user})
 
+
 @app.post("/add_user_to_db", response_class=HTMLResponse)
 async def add_user(request: Request, name: str = Form(...), email: str = Form(...), db: Session = Depends(get_db)):
     db_user = User(name=name, email=email)
@@ -43,6 +48,7 @@ async def add_user(request: Request, name: str = Form(...), email: str = Form(..
     db.commit()
     db.refresh(db_user)
     return templates.TemplateResponse("user_added.html", {"request": request, "user": db_user})
+
 
 @app.get("/add_user", response_class=HTMLResponse)
 async def add_user_form(request: Request):
